8b14a8d1e13382ccde8a466ddca4b1f02293368e
Kyle Fontenot <kylewilliamfontenot@gmail.com>
2022-04-15 10:00:17 -0700

    Upgraded Astro and ImageTools

---
+++ astro-imagetools.config.js
@@ -0,0 +1,4 @@
+export default {
+	format: "webp",
+  breakpoints: [320, 640, 1020, 1580, 2070]
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,18 @@
 import astroImagePlugin from "astro-imagetools/plugin";
-import viteCompression from "vite-plugin-compression";
+import solid from "@astrojs/solid-js";
 
-export default {
-	projectRoot: ".", // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-	pages: "./src/pages", // Path to Astro components, pages, and data
-	dist: "./dist", // When running `astro build`, path to final static output
-	public: "./public", // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-	buildOptions: {
-		// site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-		sitemap: true, // Generate sitemap (set to "false" to disable)
-	},
-	devOptions: {
-		hostname: "localhost", // The hostname to run the dev server on.
-		port: 3000, // The port to run the dev server on.
-	},
-	renderers: ["@astrojs/renderer-solid"],
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+	root: ".",
+	srcDir: "./src",
+	// pages: "./src/pages",
+	outDir: "./dist",
+	publicDir: "./public",
+	// site: "",
+	integrations: [solid()],
 	vite: {
-		plugins: [astroImagePlugin, viteCompression],
-		optimizeDeps: {
-			exclude: ["@astropub/codecs", "imagetools-core", "sharp"],
-		},
-		ssr: {
-			external: ["sharp", "potrace"],
-		},
+		plugins: [astroImagePlugin],
+		build: "esbuild",
 	},
-};
+});
